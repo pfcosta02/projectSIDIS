acd8b39bcc5d9b4c44d29e903e1113e49a2e1c63
pfcosta <1201576@isep.ipp.pt>
2022-10-19 09:36:00 +0100

    App and AppController

--- register app/src/main/java/com/example/register/controllers/AppController.java
+++ register app/src/main/java/com/example/register/controllers/AppController.java
@@ -1,4 +1,37 @@
 package com.example.register.controllers;
 
+import com.example.register.model.App;
+import com.example.register.services.AppService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Tag(name = "Application Registry", description = "Endpoints for Register Applications")
+@RestController
+@RequestMapping("/api/apps")
 public class AppController {
+
+    @Autowired
+    private AppService service;
+
+    @Operation(summary = "Shows All Apps")
+    @GetMapping
+    public List<App> findAll() {
+        return service.findAll();
+    }
+
+
+    @Operation(summary = "Register an app")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<App> createProduct(@Valid @RequestBody final App appId) {
+        final var app = service.create(appId);
+        return ResponseEntity.ok().eTag(Long.toString(app.getVersion())).body(app);
+    }
 }
--- register app/src/main/java/com/example/register/model/App.java
+++ register app/src/main/java/com/example/register/model/App.java
@@ -1,5 +1,43 @@
 package com.example.register.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.persistence.*;
+
+@Schema(description = "Applications")
+@Entity
 public class App {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long appId;
+
+    @Column (name = "URL", nullable = false)
+    private String url;
+
+    @Version
+    private Long version;
+
+
+    protected App() {}
+
+    public App(Long appId, String url) {
+        this.appId = appId;
+        this.url = url;
+    }
+
+    public Long getAppId() {
+        return appId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
