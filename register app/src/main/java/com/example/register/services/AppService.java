3c90dceeb97a51154048d901a0ae1a920f44e064
Rafael <1201674@isep.ipp.pt>
2022-10-19 09:30:04 +0100

    Service and repository created in Register App

--- register app/src/main/java/com/example/register/services/AppService.java
+++ register app/src/main/java/com/example/register/services/AppService.java
@@ -1,4 +1,13 @@
 package com.example.register.services;
 
+import com.example.register.model.App;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface AppService {
+
+    List<App> findAll();
+
+    App create(App app);
 }
--- register app/src/main/java/com/example/register/services/AppServiceImp.java
+++ register app/src/main/java/com/example/register/services/AppServiceImp.java
@@ -1,4 +1,32 @@
 package com.example.register.services;
 
-public class AppServiceImp {
+import com.example.register.model.App;
+import com.example.register.repositories.AppRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AppServiceImp implements AppService {
+    @Autowired
+    private AppRepository repository;
+
+    @Override
+    public List<App> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public App create(final App app) {
+        // construct a new object based on data received by the service to ensure domain
+        // invariants are met
+        final App obj = App.newFrom(app);
+
+        return repository.save(obj);
+    }
 }
